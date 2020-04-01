4e7745b77f8d51258caa69f26ca685df10fde3bc
Weiyi Lim <2474554l@student.gla.ac.uk>
2020-04-01 19:44:52 +0800

    Register page done

--- rating/urls.py
+++ rating/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name = 'rate-home'),
-    path('about/', views.about,name = 'rate-about')
+    path('about/', views.about,name = 'rate-about'),
 ]
