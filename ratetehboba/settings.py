6f4d4a6868a007b5f2add3a1684ed87c998609c6
weiyilimm <lwy_1011@hotmail.com>
2020-04-01 19:44:52 +0800

    Register page done

--- ratetehboba/settings.py
+++ ratetehboba/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'rating',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRIPSY_TEMPLATE_PACK = 'bootstrap4'
