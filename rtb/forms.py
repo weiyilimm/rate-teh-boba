bcc78cd87e050dcb987211a10b6dca33cc6e67c3
Karen Tada <2298995t@student.gla.ac.uk>
2020-04-04 14:23:05 +0900

    set up django app

---
+++ rtb/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from registration.forms import RegistrationForm
+from rtb.models import User, UserProfile, Business, Review, Boba, Flavour
+
+class RegisterForm(RegistrationForm):
+	is_business = forms.BooleanField(label ="Business User", initial = False, required = False)
+
+	class Meta:
+		fields = ('username', 'is_business', 'email', 'password', 'password2')
+		model = User
+	pass
+
+class UserProfileForm(forms.ModelForm):
+	image = forms.ImageField(help_text= 'Profile image:', required = False)
+	class Meta:
+		model = UserProfile
+		fields = ('image')
+
+	def __init(self, *args, **kwargs):
+		super(UserProfileForm, self).__init(*args, **kwargs)
+		self.fields['image'].widget.attrs.update({'type': image, 'accept': 'image/*'})
+
+class BusinessForm(forms.ModelForm):
+	name
+	address
+	description
+	stocks
+	slug
+
+	class Meta:
+		#
+
+	
