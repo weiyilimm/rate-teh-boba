1c7a7bb632836214b8177f1c46c0092fd618f9ba
Alan Koo <2513631k@student.gla.ac.uk>
2020-04-08 10:50:30 -0500

    Tests with changes

--- rating/tests.py
+++ rating/tests.py
@@ -1,3 +1,67 @@
 from django.test import TestCase
 
 # Create your tests here.
+import os
+import importlib
+import re
+import inspect
+import tempfile
+import rating.models
+from rating import forms
+# from populate_rating import populate
+from django.db import models
+from django.test import TestCase
+from django.conf import settings
+from django.urls import reverse, resolve
+from django.contrib.auth.models import User
+from django.forms import fields as django_fields
+
+FAILURE_HEADER = f"{os.linesep}{os.linesep}{os.linesep}================{os.linesep}RTB TEST FAILURE =({os.linesep}================{os.linesep}"
+FAILURE_FOOTER = f"{os.linesep}"
+
+f"{FAILURE_HEADER} {FAILURE_FOOTER}"
+
+class ProjectStructureTests(TestCase):
+    def setUp(self):
+        self.project_base_dir = os.getcwd()
+        self.rango_app_dir = os.path.join(self.project_base_dir, 'rating')
+
+    def test_project_created(self):
+        directory_exists = os.path.isdir(os.path.join(self.project_base_dir, 'ratetehboba'))
+        urls_module_exists = os.path.isfile(os.path.join(self.project_base_dir, 'ratetehboba', 'urls.py'))
+
+        self.assertTrue(directory_exists,
+                        f"{FAILURE_HEADER} RTB configuration directory does not exist.{FAILURE_FOOTER}")
+        self.assertTrue(urls_module_exists,
+                        f"{FAILURE_HEADER}project's urls.py module does not exist.{FAILURE_FOOTER}")
+
+    def test_is_rating_app_configured(self):
+
+        is_app_configured = 'rating' in settings.INSTALLED_APPS
+
+        self.assertTrue(is_app_configured,
+                        f"{FAILURE_HEADER}The rating app missing from  setting's INSTALLED_APPS list.{FAILURE_FOOTER}")
+    def test_is_users_app_configured(self):
+
+        is_app_configured = 'users' in settings.INSTALLED_APPS
+
+        self.assertTrue(is_app_configured,
+                        f"{FAILURE_HEADER}users app missing from setting's INSTALLED_APPS list.{FAILURE_FOOTER}")
+
+
+class ViewTests(TestCase):
+    def setUp(self):
+        self.views_module = importlib.import_module('rating.views')
+        self.views_module_listing = dir(self.views_module)
+
+        self.project_urls_module = importlib.import_module('rating.urls')
+
+    # Do some of the important views work/exist?
+    def test_view_exists(self):
+        name_exists = 'home' in self.views_module_listing
+        is_callable = callable(self.views_module.home)
+
+        self.assertTrue(name_exists,
+                        f"{FAILURE_HEADER} Home view missing {FAILURE_FOOTER}")
+        self.assertTrue(is_callable,
+                        f"{FAILURE_HEADER}Check you have defined your home() view correctly. {FAILURE_FOOTER}")
